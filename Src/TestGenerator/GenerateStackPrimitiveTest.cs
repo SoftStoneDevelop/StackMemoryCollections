f104ee5c606f84cc966a543f41faf29b3e9b0afd
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-08-28 01:32:06 +0300

    feat: test topout

--- Src/TestGenerator/GenerateStackPrimitiveTest.cs
+++ Src/TestGenerator/GenerateStackPrimitiveTest.cs
@@ -282,6 +282,7 @@ namespace TestGenerator
             StackPrimitiveIndex(in values, in builder, in stackNamespace, in toStr);
             StackPrimitivePop(in values, in builder, in stackNamespace, in toStr);
             StackPrimitiveTop(in values, in builder, in stackNamespace, in toStr);
+            StackPrimitiveTopOut(in values, in builder, in stackNamespace, in toStr);
             StackPrimitiveTopRefPtr(in values, in builder, in stackNamespace, in toStr);
             StackPrimitiveTopRefValue(in values, in builder, in stackNamespace, in toStr);
             StackPrimitiveTopPtr(in values, in builder, in stackNamespace, in toStr);
@@ -1165,6 +1166,51 @@ namespace Tests
                 builder.Append($@"
                     stack.Push({toStr(values[i])});
                     var item{i} = stack.Top();
+                    Assert.That(item{i}, Is.EqualTo({toStr(values[i])}));
+");
+            }
+
+            builder.Append($@"
+                }}
+            }}
+        }}
+");
+
+        }
+
+        private void StackPrimitiveTopOut<T>(
+            in List<T> values,
+            in StringBuilder builder,
+            in string stackNamespace,
+            in Func<T, string> toStr
+            ) where T : unmanaged
+        {
+            if (values.Count < 5)
+            {
+                throw new ArgumentException($"{nameof(values)} Must have minimum 5 values to generate tests");
+            }
+
+            builder.Append($@"
+        [Test]
+        [SkipLocalsInit]
+        public void TopOutTest()
+        {{
+            unsafe
+            {{
+                using (var memory = new StackMemoryCollections.Struct.StackMemory(sizeof({typeof(T).Name}) * {values.Count}))
+                {{
+                    var stack = new StackMemoryCollections.{stackNamespace}.Stack<{typeof(T).Name}>({values.Count}, &memory);
+                    Assert.That(() => stack.TopOut(out _),
+                        Throws.Exception.TypeOf(typeof(Exception))
+                        .And.Message.EqualTo(""There are no elements on the stack"")
+                        );
+");
+            for (int i = 0; i < values.Count; i++)
+            {
+                builder.Append($@"
+                    stack.Push({toStr(values[i])});
+                    {typeof(T).Name} item{i};
+                    stack.TopOut(out item{i});
                     Assert.That(item{i}, Is.EqualTo({toStr(values[i])}));
 ");
             }
